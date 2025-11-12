44f885065dccbe755dfeb595eb6ed75089fe4541
Oleksii Kyryk <kirikoleksiy112@gmail.com>
2025-11-12 10:40:04 +0200

    fix modal

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -11,7 +11,7 @@ interface NoteDraftStore {
 const initialDraft: PostNote = {
   title: "",
   content: "",
-  tag: "Todo",
+  tag: "",
 };
 
 export const useNoteDraftStore = create<NoteDraftStore>()(
