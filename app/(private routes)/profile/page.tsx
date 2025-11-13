55c716dd7366601e376939f29d42955fa8a75b2c
Oleksii Kyryk <kirikoleksiy112@gmail.com>
2025-11-13 12:28:22 +0200

    final

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -2,6 +2,37 @@ import Link from "next/link";
 import css from "./ProfilePage.module.css";
 import { getServerMe } from "@/lib/api/serverApi";
 import Image from "next/image";
+import { Metadata } from "next";
+import { BASE_URL } from "@/lib/api/api";
+
+export const metadata: Metadata = {
+  title: "Profile | NoteHub",
+  description:
+    "View and manage your personal profile information, update your username and avatar, or access your account settings on NoteHub.",
+  openGraph: {
+    title: "Profile | NoteHub",
+    description:
+      "Manage your personal profile, edit your details, and keep your NoteHub account up to date.",
+    url: `${BASE_URL}/profile`,
+    siteName: "NoteHub",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Profile Page",
+      },
+    ],
+    type: "profile",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Profile | NoteHub",
+    description:
+      "Manage your personal information and account details on NoteHub.",
+    images: ["https://ac.goit.global/fullstack/react/og-meta.jpg"],
+  },
+};
 
 async function ProfilePage() {
   const user = await getServerMe();
